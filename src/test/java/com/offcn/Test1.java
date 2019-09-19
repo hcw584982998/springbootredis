4559df57fe3ffac6f35685dec7d124abd318667e
hcw <haha@qq.com>
2019-09-19 20:29:27 +0800

    小白修改测试 添加测试2、3

--- src/test/java/com/offcn/Test1.java
+++ src/test/java/com/offcn/Test1.java
@@ -17,4 +17,14 @@ public class Test1 {
     public void test1(){
         redisTemplate.boundValueOps("q100").set("11111");
     }
+
+    @Test
+    public void test2(){
+        //测试2
+    }
+
+    @Test
+    public void test3(){
+        //测试3
+    }
 }
